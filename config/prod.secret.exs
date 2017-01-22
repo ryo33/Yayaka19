e1d1c714fba43da21600a317c01f2b4ebc9db952
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-01-22 18:59:28 +0900

    Add files

---
+++ config/prod.secret.exs
@@ -0,0 +1,29 @@
+use Mix.Config
+
+config :share, Share.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :share, Share.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "share_prod",
+  pool_size: 19,
+  ssl: true,
+  url: System.get_env("DATABASE_URL")
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.get_env("GOOGLE_ID"),
+  client_secret: System.get_env("GOOGLE_SECRET"),
+  redirect_uri: "https://sha.herokuapp.com/auth/google/callback"
+
+config :ueberauth, Ueberauth.Strategy.Facebook.OAuth,
+  client_id: System.get_env("FACEBOOK_ID"),
+  client_secret: System.get_env("FACEBOOK_SECRET")
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("GITHUB_ID"),
+  client_secret: System.get_env("GITHUB_SECRET")
+
+config :guardian, Guardian,
+  secret_key: System.get_env("GUARDIAN_SECRET")
