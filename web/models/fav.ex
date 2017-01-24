784a2834954b82f5a1b169d8daa9ecb9f7059b02
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-01-24 16:34:12 +0900

    Fix favs

--- web/models/fav.ex
+++ web/models/fav.ex
@@ -21,19 +21,16 @@ defmodule Share.Fav do
 
   def get_favs(socket, posts) do
     user = socket.assigns.user
-    posts = socket.assigns.posts
-    client_posts = socket.assigns.client_posts
-    posts = Enum.map(posts, fn id -> posts[id] end)
     if is_nil(user) do
       []
     else
       query = from p in Share.Post,
-        left_join: f in Share.Fav,
-          on: f.post_id == p.id and f.user_id == ^user.id,
+        left_join: f in Share.Fav, on: f.post_id == p.id and f.user_id == ^user.id,
         where: not is_nil(f.id),
+        where: p.id in ^posts,
         select: p.id
       Share.Repo.all(query)
-      |> Enum.map(fn id -> client_posts[id] end)
+      |> Enum.map(fn id -> socket.assigns.client_posts[id] end)
     end
   end
 end
