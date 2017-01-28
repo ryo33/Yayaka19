a1c0737b17c36e888d38c4a6afb21b1909fdc116
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-01-28 14:33:00 +0900

    Enhance the login page

--- web/static/js/components/LoginPage.js
+++ web/static/js/components/LoginPage.js
@@ -5,13 +5,13 @@ import { Button, Segment } from 'semantic-ui-react'
 export default () => (
   <Segment.Group>
     <Segment>
-      <Button primary size='massive' href="/auth/twitter">Sign in with Twitter</Button>
+      <Button primary size='large' href="/auth/twitter">Sign in with Twitter</Button>
     </Segment>
     <Segment>
-      <Button primary size='massive' href="/auth/facebook">Sign in with Facebook</Button>
+      <Button primary size='large' href="/auth/facebook">Sign in with Facebook</Button>
     </Segment>
     <Segment>
-      <Button primary size='massive' href="/auth/github">Sign in with GitHub</Button>
+      <Button primary size='large' href="/auth/github">Sign in with GitHub</Button>
     </Segment>
     <Segment>
       <a href="/auth/google" id="sign-in-with-google"></a>
