c43eaed411efb15f2da4b686b63437cf79a53229
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-02-09 16:08:42 +0900

    Enhance appearance and trifling behavior

--- web/static/js/components/PublicTimeline.js
+++ web/static/js/components/PublicTimeline.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { Segment, Message, Header, Button, Label } from 'semantic-ui-react'
+import { Segment, Message, Header, Button, Label, Dimmer, Loader } from 'semantic-ui-react'
 
 import { signedIn } from '../global.js'
-import { userSelector, publicTimelinePostsSelector } from '../selectors.js'
+import { userSelector, publicTimelineSelector } from '../selectors.js'
 import { requestPublicTimeline } from '../actions.js'
 
 import PostList from './PostList.js'
 
 const mapStateToProps = state => {
+  const {
+    posts, isLoadingPublicTimeline
+  } = publicTimelineSelector(state)
   return {
     user: userSelector(state),
-    posts: publicTimelinePostsSelector(state)
+    posts, isLoadingPublicTimeline
   }
 }
 
@@ -20,31 +23,51 @@ const actionCreators = {
   requestPublicTimeline
 }
 
-const PublicTimeline = ({ posts, requestPublicTimeline, user }) => (
-  <div>
-    { signedIn ? null : (
-      <Message>
-        <Message.Header>
-          Welcome!
-        </Message.Header>
-        <p>
-          This is a social networking service.<br />
-          You need to login to post, follow users, use personal timeline, favorite posts, etc.
-        </p>
-      </Message>
-    )}
-    <Segment vertical>
-      <Header>Public Timeline</Header>
-      <Button primary onClick={requestPublicTimeline}>Reload</Button>
-      <Label size='large'>{user.display} @{user.name}</Label>
-    </Segment>
-    <PostList
-      posts={posts}
-    />
-    <Segment vertical>
-      <Button onClick={requestPublicTimeline}>Reload</Button>
-    </Segment>
-  </div>
-)
+class PublicTimeline extends Component {
+  constructor(props) {
+    super(props)
+    this.handleReload = this.handleReload.bind(this)
+  }
+
+  handleReload() {
+    const { requestPublicTimeline } = this.props
+    requestPublicTimeline()
+  }
+
+  render() {
+    const {
+      posts, requestPublicTimeline, user, isLoadingPublicTimeline
+    } = this.props
+    return (
+      <div>
+        <Dimmer active={isLoadingPublicTimeline} inverted>
+          <Loader inverted />
+        </Dimmer>
+        { signedIn ? null : (
+          <Message>
+            <Message.Header>
+              Welcome!
+            </Message.Header>
+            <p>
+              This is a social networking service.<br />
+              You need to login to post, follow users, use personal timeline, favorite posts, etc.
+            </p>
+          </Message>
+        )}
+        <Segment vertical>
+          <Header>Public Timeline</Header>
+          <Button primary onClick={requestPublicTimeline}>Reload</Button>
+          <Label size='large'>{user.display} @{user.name}</Label>
+        </Segment>
+        <PostList
+          posts={posts}
+        />
+        <Segment vertical>
+          <Button onClick={requestPublicTimeline}>Reload</Button>
+        </Segment>
+      </div>
+    )
+  }
+}
 
 export default connect(mapStateToProps, actionCreators)(PublicTimeline)
--- web/static/js/reducers/userPage.js
+++ web/static/js/reducers/userPage.js
@@ -4,16 +4,16 @@ import { createReducer } from 'redux-act'
 import { setUserInfo } from '../actions.js'
 
 const user = createReducer({
-  [setUserInfo]: (state, { user }) => user
+  [setUserInfo]: (state, { user = null }) => user
 }, null)
 const postCount = createReducer({
-  [setUserInfo]: (state, { postCount }) => postCount
+  [setUserInfo]: (state, { postCount = 0 }) => postCount
 }, 0)
 const following = createReducer({
-  [setUserInfo]: (state, { following }) => following
+  [setUserInfo]: (state, { following = 0 }) => following
 }, 0)
 const followers = createReducer({
-  [setUserInfo]: (state, { followers }) => followers
+  [setUserInfo]: (state, { followers = 0 }) => followers
 }, 0)
 
 export default combineReducers({
