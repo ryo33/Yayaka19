185145eb92fff3966141e695cb2a96e567b990a4
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-02-09 16:30:26 +0900

    Add load-recent-posts button to user page

--- web/static/js/reducers/userPage.js
+++ web/static/js/reducers/userPage.js
@@ -1,24 +1,40 @@
 import { combineReducers } from 'redux'
 import { createReducer } from 'redux-act'
 
-import { setUserInfo } from '../actions.js'
+import {
+  setUserInfo, requestUserPosts, setUserPosts
+} from '../actions.js'
 
 const user = createReducer({
   [setUserInfo]: (state, { user = null }) => user
 }, null)
+
 const postCount = createReducer({
   [setUserInfo]: (state, { postCount = 0 }) => postCount
 }, 0)
+
 const following = createReducer({
   [setUserInfo]: (state, { following = 0 }) => following
 }, 0)
+
 const followers = createReducer({
   [setUserInfo]: (state, { followers = 0 }) => followers
 }, 0)
+
+const isLoadingUserPosts = createReducer({
+  [requestUserPosts]: () => true,
+  [setUserPosts]: () => false
+}, false)
+
+const posts = createReducer({
+  [setUserPosts]: (state, posts) => posts
+}, null)
 
 export default combineReducers({
   user,
   postCount,
   following,
-  followers
+  followers,
+  isLoadingUserPosts,
+  posts
 })
