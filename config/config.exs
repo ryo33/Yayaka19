2c5e8966c4ece4aaab7d22585c97ac749c36332f
Ryo Hashiguchi <ryo12redstone@gmail.com>
2017-02-15 17:11:33 +0900

    Change the site name to Yayaka19

--- config/config.exs
+++ config/config.exs
@@ -18,14 +18,14 @@ config :share, Share.Endpoint,
            adapter: Phoenix.PubSub.PG2]
 
 config :share,
-  title: "Share",
+  title: "Yayaka19",
   description: "What's in your head?",
-  url: "https://sha.herokuapp.com",
-  hashtag: "shaherokuapp",
+  url: "https://yayaka.net",
+  hashtag: "Yayaka19",
   admin: [name: "Ryo33",
            url: "https://twitter.com/ryo33music"],
-  source: [url: "https://github.com/ryo33/share",
-           title: "https://github.com/ryo33/share"],
+  source: [url: "https://github.com/ryo33/Yayaka19",
+           title: "https://github.com/ryo33/Yayaka19"],
   api: [url: "https://gist.github.com/ryo33/145e5ef24bad6f11abbb902edc6979d6",
         title: "https://gist.github.com/ryo33/145e5ef24bad6f11abbb902edc6979d6"]
 
@@ -46,7 +46,7 @@ config :ueberauth, Ueberauth,
 config :guardian, Guardian,
   allowed_algos: ["HS512"], # optional
   verify_module: Guardian.JWT,  # optional
-  issuer: "Share",
+  issuer: "Yayaka19",
   ttl: { 30, :days },
   verify_issuer: true, # optional
   secret_key: System.get_env("GUARDIAN_SECRET_KEY") || "abcdef",
